7eb7d108b3c72d6f379fc3cf081ee7ef8ffe24ec
Таланов Денис <denist.talanov.gmail.com@mail.ru>
2024-11-24 17:23:08 +0300

    added bookings endpoint

---
+++ app/bookings/router.py
@@ -0,0 +1,29 @@
+from datetime import date
+
+from fastapi import APIRouter, Request
+from fastapi.params import Depends
+
+from app.bookings.dao import BookingDAO
+from app.bookings.shemas import SBooking
+from app.exceptions import RoomCannotBeBooked
+from app.users.users_dependencies import get_current_user
+from app.users.users_models import Users
+
+router = APIRouter(prefix="/bookings", tags=["Бронь"])
+
+
+@router.get("")
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBooking]:
+    return await BookingDAO.find_all(user_id=user.id)
+
+
+@router.post("/add_booking")
+async def add_booking(
+    room_id: int,
+    date_from: date,
+    date_to: date,
+    user: Users = Depends(get_current_user),
+):
+    booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
+    if not booking:
+        raise RoomCannotBeBooked
